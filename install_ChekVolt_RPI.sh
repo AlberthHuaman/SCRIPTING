a3291a8a0be289a30ce73d7c28de2968cbbd9f4b
AlberthHuaman <140645451+AlberthHuaman@users.noreply.github.com>
2024-05-27 15:28:37 -0500

    Update install_ChekVolt_RPI.sh

--- install_ChekVolt_RPI.sh
+++ install_ChekVolt_RPI.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
 # Variables
-FILE_ID="tu_id_de_archivo"
-FILENAME="nombre_del_archivo.py"
-FOLDER_NAME="mi_carpeta"
-NEW_FILE="archivo.txt"
+PATH_FOLDER1="/opt/demonCheckHealth_RPI/"
+FILE_CHECK="CheckVolt_script.py"
+PATH_FOLDER2="/etc/systemd/system/"
+FILE_SERVICE="checkhealth.service"
+FILE_LOG="checkhealth.log"
 
-# Descargar el archivo desde Google Drive
-echo "Descargando archivo desde Google Drive..."
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$FILE_ID -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p')
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILENAME
-rm -rf /tmp/cookies.txt
-echo "Archivo descargado: $FILENAME"
+# Crear la carpeta & archivos
+sudo mkdir /opt/demonCheckHealth_RPI/
+sudo touch /var/log/$FILE_LOG
+sudo chmod 666 /var/log/$FILE_LOG
 
-# Crear la carpeta
-echo "Creando la carpeta..."
-mkdir -p $FOLDER_NAME
-echo "Carpeta creada: $FOLDER_NAME"
+# Descargar el archivo 
+sudo wget "https://raw.githubusercontent.com/AlberthHuaman/SCRIPTING/main/CheckVolt_script.py" -O $PATH_FOLDER1/$FILE_CHECK && sudo chmod +x $PATH_FOLDER1/$FILE_CHECK
+sudo wget "https://raw.githubusercontent.com/AlberthHuaman/SCRIPTING/main/checkhealth.service" -O $PATH_FOLDER2/$FILE_SERVICE && sudo chmod +x $PATH_FOLDER2/$FILE_SERVICE
 
-# Crear un archivo dentro de la carpeta
-echo "Creando el archivo..."
-touch $FOLDER_NAME/$NEW_FILE
-echo "Archivo creado: $FOLDER_NAME/$NEW_FILE"
-
-# Asignar permisos de ejecución al archivo
-echo "Asignando permisos de ejecución..."
-chmod +x $FOLDER_NAME/$NEW_FILE
-echo "Permisos asignados: $FOLDER_NAME/$NEW_FILE"
+#Activación de servicio
+sudo systemctl enable $FILE_SERVICE
+sudo systemctl start $FILE_SERVICE
 
 echo "Script completado con éxito."
+
+#python3 $PATH_FOLDER1/$FILE_CHECK &
+
+
+
